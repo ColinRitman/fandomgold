51ebf8f53527b401a20360aab33fa4107f30b932
ColinRitman <54386876+ColinRitman@users.noreply.github.com>
2019-11-18 04:32:42 -0500

    Limit max block req
    Referencing HackerOne report #506595
    Thanks & credit given to cash2, aphivantrakul, 'cryptozoidberg', 'moneromooo'

--- src/CryptoNoteProtocol/CryptoNoteProtocolHandler.cpp
+++ src/CryptoNoteProtocol/CryptoNoteProtocolHandler.cpp
@@ -1,6 +1,8 @@
 // Copyright (c) 2012-2016, The CryptoNote developers, The Bytecoin developers
 // Copyright (c) 2014-2018, The Forknote project
 // Copyright (c) 2016-2018, The Karbowanec developers
+// Copyright (c) 2018-2019, The Cash2 developers
+// Copyright (c) 2018-2019, The Fandom Gold Project
 //
 // This file is part of Bytecoin.
 //
